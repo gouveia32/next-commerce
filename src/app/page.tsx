9d9808df88ddf018380b01558a9f78419dd2ee51
Jose Alves de Gouveia <gouveia32@gmail.com>
2024-06-04 11:36:36 -0300

    customizando o componente image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,36 @@
 import Image from "next/image";
+import { ProductType } from "@/types/Producttype";
+import Product from "./components/Product";
 
-export default function Home() {
+
+//https://fakestoreapi.com/products
+
+async function getData() {
+  const res = await fetch('https://fakestoreapi.com/products')
+ 
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
+
+
+export default async function Home() {
+  const products = await getData();
+
+  //console.log(products)
   return (
-    <h1>Next-Commerce</h1>
+    <div className="max-w-7xl mx-auto pt-8 px-8 xl:px-0">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-10 xl:gap-6">
+        
+        { products.map((product: ProductType) => (
+          <Product key={product.id} product={product}></Product>
+        )) }
+
+      </div>
+    </div>
    
   )
 }
